97e1d917b303dea68ec4dab577dcc37af51ae850
sohal rahaman <sohalr1305@gmail.com>
2023-10-15 00:34:11 +0530

    database connection

--- server/index.js
+++ server/index.js
@@ -1,13 +1,22 @@
 import express from "express";
 import dotenv from 'dotenv'
+import mongoose from "mongoose";
 
-dotenv.config({path:"server/index.js"});
+dotenv.config();
+
+//database connection
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>{
+    console.log("DataBase is Now Connected");
+}).catch((err)=>{
+    console.log(err);
+})
 
 const app = express()
 
 
 
-const PORT=process.env.PORT || 3000;
+const PORT=process.env.PORT;
 
 app.listen(PORT,()=>{
     console.log(`server working at ${PORT}`);
